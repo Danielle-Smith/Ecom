a1e2386920d75ec9b22bf0e3675f42da7ba66dab
Danielle <danielle@natesdesign.com>
2020-07-18 13:04:38 -0600

    built out layout rid and header navbar components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './headernavbar/header';
+import Navbar from './headernavbar/navbar';
+
 class Layout extends Component {
   render() {
     return (
       <div className='layout'>
-        <h1>DevCamp React Starter</h1>
+        <Header />
+        <Navbar />
         {this.props.children}
       </div>
     );
