eea2a2deea80be8cb0f8c99aec95cd37f0275790
Danielle <danielle@natesdesign.com>
2020-08-04 23:21:04 -0600

    built account information from signinform as a template

--- src/components/account/accountInformation.js
+++ src/components/account/accountInformation.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+import PageTitle from '../pageTitle';
+import AccountInformationForm from './accountInformationForm';
+
 class AccountInformation extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div>
-                account information
+            <div className='account-information'>
+                <Pagetitle className='account-information' title='Account Information' />
+                <AccountInformationForm onSubmit={thisonSubmit} className='account-information__form' />
             </div>
         )
     }
