25b4a4d846d9852699bfaf872631590d1c035a8a
Danielle <danielle@natesdesign.com>
2020-08-15 15:53:04 -0600

    filtered products feature complete

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
-
-
+import ShopSearchBar from './shopSearchBar';
+import ShopProduct from './shopProduct';
+import ShopCart from './shopCart';
+import CartButton from './cartButton';
 
 class Shop extends Component {
 
