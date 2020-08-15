e3170ef18768b38bc66bef8b994ae4517713fbd0
Danielle <danielle@natesdesign.com>
2020-08-15 17:05:41 -0600

    search bar styles

--- src/components/shop/shopSearchBar.js
+++ src/components/shop/shopSearchBar.js
@@ -4,7 +4,10 @@ import { reduxForm, Field } from 'redux-form';
 
 function FormSearchBar(props) {
     return (
-        <input className={ `${props.className} form-search-bar`} {...props.input} type='text' placeholder={`${props.placeholder}`} />
+        <div className='search-bar-grid'>
+            <i className="fas fa-search search-bar-grid__icon"></i>
+            <input className={ `${props.className} form-search-bar`} {...props.input} type='text' placeholder={`${props.placeholder}`} />
+        </div>
     )
 }
 
@@ -13,7 +16,7 @@ class ShopSearchBar extends Component {
         const { className, handleSubmit } = this.props;
         return (
             <Form onSubmit={handleSubmit} className={`${className} shop-search-bar`}>
-                <Field name='query' className='shop-seach-bar__form-seach-bar' placeholder='search' component={FormSearchBar} />
+                <Field name='query' className='shop-seach-bar__form-seach-bar' placeholder='Search' component={FormSearchBar} />
             </Form>
         )
     }
