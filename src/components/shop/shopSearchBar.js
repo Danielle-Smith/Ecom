adabe769b7597cfc1fb8498299ec8406cfda6cc7
Danielle <danielle@natesdesign.com>
2020-08-15 16:22:03 -0600

    built shop search bar form and form field

---
+++ src/components/shop/shopSearchBar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+function FormSearchBar(props) {
+    return (
+        <input className={ `${props.className} form-search-bar`} {...props.input} type='text' placeholder={`${props.placeholder}`} />
+    )
+}
+
+class ShopSearchBar extends Component {
+    rener() {
+        const { className, handleSubmit } = this.props;
+        return (
+            <Form onSubmit={handleSubmit} className={`${className} shop-search-bar`}>
+                <Field name='shop-search-bar' className='shop-seach-bar__form-seach-bar' placeholder='search' component={FormSearchBar} />
+            </Form>
+        )
+    }
+}
+
+ShopSearchBar = reduxForm({
+    form: 'ShopSearchBar'
+})(ShopSearchBar);
+
+export default ShopSearchBar;
