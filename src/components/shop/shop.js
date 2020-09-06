04547579e99ffb8f30a30e57bf7c9f7a0605856a
Danielle <danielle@natesdesign.com>
2020-09-06 15:04:14 -0600

    cart animations and cart button in shop component

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -5,7 +5,7 @@ import * as actions from '../../actions';
 import ShopSearchBar from './shopSearchBar';
 import ShopProduct from './shopProduct';
 import ShopCart from './shopCart';
-// import CartButton from './cartButton';
+import CartButton from './cartButton';
 
 class Shop extends Component {
 
@@ -71,7 +71,7 @@ class Shop extends Component {
                     this.state.showCart ? <ShopCart className='shop__cart'/> : ''
                 } 
                 
-                {/* <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus'/> */}
+                <CartButton onClick={this.handleAddToCart} className='shop__cart-button' icon='fas fa-cart-plus'/>
             </div>
         )
     }
