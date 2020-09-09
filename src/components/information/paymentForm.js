835816dc1d0e0225a40cd5049286bbef708df836
Danielle <danielle@natesdesign.com>
2020-09-09 10:28:49 -0600

    built shipping to component

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -54,6 +54,11 @@ class PaymentForm extends Component {
                 component={FormButton}/>
 
                 <OrderSummary className='payment-form__order-summary' />
+                <div className='payment-form__shipping-info shipping-info'>
+                    <UnderlinedTitle className='shipping-info__title' title='Shipping To' />
+                    <div className='shipping-info__name small-text'>Jordan Hudgens</div>
+                    <div calssName='shipping-info__address small-text'>1234 address goes here</div>
+                </div>
             </form>
         )
     }
